018b3780e80d949129fd035f4a9499a71a29c319
AMANKANOJIYA <50018596+AMANKANOJIYA@users.noreply.github.com>
2020-10-01 21:13:09 +0530

    updating program
    adding comments to you programes for more precise understanding

--- bmicalculator.js
+++ bmicalculator.js
@@ -1,14 +1,18 @@
+/* BMI Calculator function*/
 function bmicalc(weight,height)
 {
     var bmi=weight/(height*height);
     return Math.round(bmi);
 }
 
+/* Accept input variables*/
 var w=prompt("Enter Your Weight : ");
 var h=prompt("Enter Your Height : ");
 
+/* Performing calculations */	
 var bmi = bmicalc(w,h);
 
+/* Interpretation of BMI value */
 if(bmi<18.5)
 {
     yourbmi="Underweight";
@@ -21,5 +25,5 @@ else
 {
     yourbmi="Overweight";
 }
-
+/*out put through Alert*/
 alert("Hello, your BMI is "+bmi+" it's "+yourbmi);
